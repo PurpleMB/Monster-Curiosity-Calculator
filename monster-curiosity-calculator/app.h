9ec1966af49aeb60994943da3deb91c395d11879
PurpleMB <purplemb816@gmail.com>
2025-05-28 15:50:52 -0500

    imported dearimgui library and connected to project

---
+++ monster-curiosity-calculator/app.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "imgui.h"
+#include "imgui_impl_dx9.h"
+#include "imgui_impl_win32.h"
+#include <d3d9.h>
+#include <tchar.h>
+#include <cstdlib>
+
+class App
+{
+public:
+	App();
+	virtual ~App();
+	void Run();
+	virtual void StartUp();
+	virtual void Update();
+
+protected:
+	ImVec4 clear_color;
+	ImGuiIO* io;
+private:
+	HWND hwnd;
+	WNDCLASSEXW wc;
+};
+
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void ResetDevice();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
