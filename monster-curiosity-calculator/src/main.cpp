f4d51be04d5e09eab64c3bb599b99f1ca2c43940
PurpleMB <purplemb816@gmail.com>
2025-05-30 13:13:01 -0500

    defining window placement constants. beginning development of type selection window

--- monster-curiosity-calculator/src/main.cpp
+++ monster-curiosity-calculator/src/main.cpp
@@ -8,6 +8,10 @@ public:
     const std::string DB_PATH = "c:\\DB_TEST\\test.db";
     const std::string JSON_PATH = "c:\\DB_TEST\\mccdata.json";
 
+    const int DEFAULT_WINDOW_SETTINGS = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
+
+    const float MARGIN = 25;
+
     MCCApp() = default;
     ~MCCApp() = default;
 
@@ -22,8 +26,15 @@ public:
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
 
-        // calculation selection window
+        std::vector<float> window_size = { 350, 0 };
+        std::vector<float> window_pos = { MARGIN, MARGIN };
+
+        // database generation window
         {
+            ImGui::SetNextWindowSize({ window_size[0], window_size[1]});
+            ImGui::SetNextWindowPos({window_pos[0], window_pos[1]});
+
+            ImGui::Begin("Database Generation", nullptr, DEFAULT_WINDOW_SETTINGS);
             if (ImGui::Button("Build Monster Database")) {
                 createDatabase(DB_PATH.c_str());
                 deleteTable(DB_PATH.c_str());
@@ -33,40 +44,45 @@ public:
                 clearTable(DB_PATH.c_str());
                 insertDataFromJson(DB_PATH.c_str(), JSON_PATH.c_str());
             }
-            ImGui::SameLine();
-            ImGui::Text("THIS IS A BUILD TEST for basic SQLite/C++ integration");
-        }
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+            window_pos[1] += MARGIN + ImGui::GetWindowHeight();
 
-            ImGui::Begin("Monster Information");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io->Framerate, io->Framerate);
             ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
+        // type selection window
         {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+            static int selected_type = -1;
+            const char* types[] = {"Any", "None", "Normal", "Grass" , "Water", "Fire"};
+            
+            std::string selected_type_name = (selected_type == -1) ? "Any" : types[selected_type];
+
+            ImGui::SetNextWindowSize({ window_size[0], window_size[1] });
+            ImGui::SetNextWindowPos({ window_pos[0], window_pos[1] });
+
+            ImGui::Begin("Type Selection", nullptr, DEFAULT_WINDOW_SETTINGS);
+
+            ImGui::Text("Select type to search for:");
+
+            ImGui::Text("Primary Type: ");
+            ImGui::SameLine();
+            if (ImGui::Button(selected_type_name.c_str()))
+            {
+                ImGui::OpenPopup("Select type");
+            }
+
+            if (ImGui::BeginPopup("Select type"))
+            {
+                for (int i = 0; i < IM_ARRAYSIZE(types); i++)
+                {
+                    if (ImGui::Selectable(types[i]))
+                    {
+                        selected_type = i;
+                    }
+                }
+                ImGui::EndPopup();
+            }
+
             ImGui::End();
         }
     }
