2c8fadade4ffa3e7df7c1b8f0f58f402de67c6ac
PurpleMB <purplemb816@gmail.com>
2025-06-17 14:04:07 -0500

    Created basic query parameter struct

---
+++ monster-curiosity-calculator/src/db/structs_database.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+namespace monster_calculator {
+
+typedef struct QueryParameter {
+	std::string parameter_name = "";
+	std::string parameter_value = "";
+};
+
+} // namespace monster_calculator
