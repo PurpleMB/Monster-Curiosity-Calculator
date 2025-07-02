0665952b7685932b829c5d2181e9ebe4c11744c8
PurpleMB <purplemb816@gmail.com>
2025-07-02 14:09:34 -0500

    overhauled api data gathering to consider all forms, now to reprune all data

--- monster-curiosity-calculator/py/pokeapi_parser.py
+++ monster-curiosity-calculator/py/pokeapi_parser.py
@@ -7,44 +7,52 @@ import requests
 base_url = "https://pokeapi.co/api/v2/"
 
 def gather_species_data():
-    species_url = f"{base_url}/pokemon-species"
-    api_response = requests.get(species_url)
+    pokedex_url = f"{base_url}/pokemon?limit=100000&offset=0"
+    api_response = requests.get(pokedex_url)
 
     if api_response.status_code != 200:
-        print(f"API Request for species info failed. Error code {api_response.status_code}")
+        print(f"API Request for dex info failed. Error code {api_response.status_code}")
         return None
     
-    species_data = api_response.json()
-    return species_data
+    pokedex_data = api_response.json()
+    return pokedex_data
 
 async def gather_pokemon_data(session, species_data):
-    species_count = species_data["count"]
     tasks = []
-    for poke_id in range(species_count):
-        task = asyncio.create_task(get_pokemon_info(session, poke_id + 1))
+    for dex_entry in species_data["results"]:
+        task = asyncio.create_task(get_pokemon_info(session, dex_entry))
         tasks.append(task)
 
     pokedex_data = await asyncio.gather(*tasks)
     return pokedex_data
 
-async def get_pokemon_info(session, pokemon_id):
-    poke_url = f"{base_url}/pokemon/{pokemon_id}"
-    async with session.get(poke_url) as api_response:
-        if api_response.status != 200:
-            print(f"API Request for {pokemon_id} failed. Error code {api_response.status_code}")
+async def get_pokemon_info(session, dex_entry):
+    form_url = dex_entry["url"]
+    async with session.get(form_url) as form_response:
+        if form_response.status != 200:
+            print(f"API Request for {dex_entry["name"]} failed. Error code {api_response.status_code}")
             return None
+        form_data = await form_response.json()
 
-        pokemon_data = await api_response.json()
-        pruned_data = prune_pokemon_info(pokemon_data)
-        return pruned_data
+    species_url = form_data["species"]["url"]
+    async with session.get(species_url) as species_response:
+        if species_response.status != 200:
+            print(f"API Request for {form_data["species"]["name"]} failed. Error code {species_response.status_code}")
+            return None
+        species_data = await species_response.json()
 
-def prune_pokemon_info(pokemon_info):
+    pruned_species_data = prune_pokemon_info(species_data,form_data)
+    return pruned_species_data
+        
+
+def prune_pokemon_info(species_info, form_info):
     pruned_info = {}
 
-    # basic identification info
-    pruned_info["name"] = pokemon_info["name"]
-    pruned_info["id"] = pokemon_info["id"]
 
+    # basic identification info
+    pruned_info["name"] = form_info["name"]
+    pruned_info["dex_number"] = species_info["id"]
+    '''
     # size info
     pruned_info["weight(kg)"] = pokemon_info["weight"] / 10
     pruned_info["height(m)"] = pokemon_info["height"] / 10
@@ -79,7 +87,7 @@ def prune_pokemon_info(pokemon_info):
     poke_abilities["hidden"] = abilities[2]
 
     pruned_info["abilities"] = poke_abilities
-
+    '''
     return pruned_info
 
 
