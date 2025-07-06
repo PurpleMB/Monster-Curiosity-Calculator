44542e906fb7f6d08a714c62096d193304d2dd35
PurpleMB <purplemb816@gmail.com>
2025-07-06 18:28:28 -0500

    overhauled subset display table to use parameter table format

--- monster-curiosity-calculator/src/mcc_gui_window.cpp
+++ monster-curiosity-calculator/src/mcc_gui_window.cpp
@@ -239,75 +239,33 @@ void DrawSetDisplayWindow(WindowParameters& window_parameters, OutputEnvironment
 	std::string subset_size_text = "Subset Size: " + std::to_string(output_environment.subset_entries.size());
 	ImGui::Text(subset_size_text.c_str());
 
-	// subset table sorting fields
-	ImGui::Text("Sort entries by:");
-
-	std::vector<std::string> sortable_parameters = {"ID #", "Name", "Stat Total"};
-	std::vector<std::string> sortable_query_params = {"dex_number", "name", "stat_total"};
-	static int selected_parameter_index = 0;
-	std::string selected_sorting_param_name = sortable_parameters[selected_parameter_index];
-
-	std::vector<std::string> sortable_directions = {"Ascending", "Descending"};
-	std::vector<std::string> sortable_query_directions = {"asc", "desc"};
-	static int selected_direction_index = 0;
-	std::string selected_sorting_name = sortable_directions[selected_direction_index];
-	
-	if (ImGui::Button(selected_sorting_param_name.c_str())) {
-		ImGui::OpenPopup("Select sorting parameter");
-	}
-	ImGui::SameLine();
-	if (ImGui::Button(selected_sorting_name.c_str())) {
-		ImGui::OpenPopup("Select sorting direction");
-	}
-	ImGui::SameLine();
-	if (ImGui::Button("Apply")) {
-		/*
-		std::string selected_query_param = sortable_query_params[selected_parameter_index];
-		std::string selected_query_dir = sortable_query_directions[selected_direction_index];
-		QueryParameter sorting_param(selected_query_param, selected_query_dir);
-		output_environment.sorting_parameter = sorting_param;
-		SortSubtableEntries(output_environment);
-		*/
-	}
-
-	if (ImGui::BeginPopup("Select sorting parameter")) {
-		for (int i = 0; i < sortable_parameters.size(); i++) {
-			if (ImGui::Selectable(sortable_parameters[i].c_str())) {
-				selected_parameter_index = i;
-			}
-		}
-		ImGui::EndPopup();
-	}
-	if (ImGui::BeginPopup("Select sorting direction")) {
-		for (int i = 0; i < sortable_directions.size(); i++) {
-			if (ImGui::Selectable(sortable_directions[i].c_str())) {
-				selected_direction_index = i;
-			}
-		}
-		ImGui::EndPopup();
-	}
-
-
 	// subset display table
-	ImVec2 outer_size = ImVec2(0.0f, 300.0f);
-	const int kColumnCount = 3;
+	ImVec2 outer_size = ImVec2(0.0f, 400.0f);
+	const int kColumnCount = 1;
 	const int kTableFlags = ImGuiTableFlags_Borders |
-		ImGuiTableFlags_SizingStretchSame |
+		ImGuiTableFlags_SizingFixedFit |
 		ImGuiTableFlags_ScrollY;
+	static int frozen_columns = 0;
+	static int frozen_rows = 1;
 	if (ImGui::BeginTable("subset_entries", kColumnCount, kTableFlags, outer_size)) {
-		int subset_size = output_environment.subset_entries.size();
-		int subset_index = 0;
-		while(subset_index < subset_size) {
-		//for (std::string subset_entry : output_environment.subset_entries) {
+		// column setup
+		ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupScrollFreeze(frozen_columns, frozen_rows);
+
+		// creating label row
+		ImGui::TableNextRow(ImGuiTableRowFlags_Headers);
+		for (int column = 0; column < kColumnCount; column++) {
+			ImGui::TableSetColumnIndex(column);
+			const char* column_name = ImGui::TableGetColumnName(column);
+			ImGui::TableHeader(column_name);
+		}
+
+		// printing entry rows
+		for (std::string subset_entry : output_environment.subset_entries) {
 			ImGui::TableNextRow();
-			for (int column_index = 0; column_index < 3; column_index++) {
+			for (int column_index = 0; column_index < kColumnCount; column_index++) {
 				ImGui::TableSetColumnIndex(column_index);
-				std::string subset_entry = output_environment.subset_entries[subset_index];
 				ImGui::Text(subset_entry.c_str());
-				subset_index++;
-				if (subset_index >= subset_size) {
-					break;
-				}
 			}
 		}
 		ImGui::EndTable();
