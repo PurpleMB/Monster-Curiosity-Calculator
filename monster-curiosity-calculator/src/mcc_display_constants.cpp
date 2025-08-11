b91d7114c296ed7899a7a05bf177e49e725c66f7
PurpleMB <purplemb816@gmail.com>
2025-08-11 13:07:19 -0500

    added 2 additional DisplayColors per category

--- monster-curiosity-calculator/src/mcc_display_constants.cpp
+++ monster-curiosity-calculator/src/mcc_display_constants.cpp
@@ -13,17 +13,42 @@ extern const DisplayColor kRedColor = DisplayColor(
 	"Red",
 	ImVec4(255.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kVermillionColor = DisplayColor(
+	"Vermillion",
+	ImVec4(227.0f / 255.0f, 66.0f / 255.0f, 52.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kRubyRedColor = DisplayColor(
+	"Ruby Red",
+	ImVec4(224.0f / 255.0f, 17.0f / 255.0f, 95.0f / 255.0f, kColorIntensity)
+);
 
 // Blues
 extern const DisplayColor kBlueColor = DisplayColor(
 	"Blue",
 	ImVec4(0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kPeriwinkleColor = DisplayColor(
+	"Periwinkle",
+	ImVec4(204.0f / 255.0f, 204.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kIndigoColor = DisplayColor(
+	"Indigo",
+	ImVec4(63.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
+);
+
 
 // Greens
 extern const DisplayColor kGreenColor = DisplayColor(
 	"Green",
 	ImVec4(0.0f / 255.0f, 255.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kCeladonColor = DisplayColor(
+	"Celadon",
+	ImVec4(175.0f / 255.0f, 225.0f / 255.0f, 175.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kViridianColor = DisplayColor(
+	"Viridian",
+	ImVec4(64.0f / 255.0f, 130.0f / 255.0f, 109.0f / 255.0f, kColorIntensity)
 );
 
 // Yellows
@@ -31,11 +56,27 @@ extern const DisplayColor kYellowColor = DisplayColor(
 	"Yellow",
 	ImVec4(255.0f / 255.0f, 255.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kSaffronColor = DisplayColor(
+	"Saffron",
+	ImVec4(244.0f / 255.0f, 196.0f / 255.0f, 48.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kGoldenrodColor = DisplayColor(
+	"Goldenrod",
+	ImVec4(218.0f / 255.0f, 165.0f / 255.0f, 32.0f / 255.0f, kColorIntensity)
+);
 
 // Oranges
 extern const DisplayColor kOrangeColor = DisplayColor(
 	"Orange",
 	ImVec4(255.0f / 255.0f, 165.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kPersimmonColor = DisplayColor(
+	"Persimmon",
+	ImVec4(236.0f / 255.0f, 88.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kTangerineColor = DisplayColor(
+	"Tangerine",
+	ImVec4(240.0f / 255.0f, 128.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
 );
 
 // Purples
@@ -43,11 +84,27 @@ extern const DisplayColor kPurpleColor = DisplayColor(
 	"Purple",
 	ImVec4(128.0f / 255.0f, 0.0f / 255.0f, 128.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kMauveColor = DisplayColor(
+	"Mauve",
+	ImVec4(224.0f / 255.0f, 176.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kOrchidColor = DisplayColor(
+	"Orchid",
+	ImVec4(218.0f / 255.0f, 112.0f / 255.0f, 214.0f / 255.0f, kColorIntensity)
+);
 
 // Pinks
 extern const DisplayColor kPinkColor = DisplayColor(
 	"Pink",
 	ImVec4(255.0f / 255.0f, 192.0f / 255.0f, 203.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kRaspberryColor = DisplayColor(
+	"Raspberry",
+	ImVec4(227.0f / 255.0f, 11.0f / 255.0f, 92.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kFuschiaColor = DisplayColor(
+	"Fuschia",
+	ImVec4(255.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
 );
 
 // Whites
@@ -55,17 +112,41 @@ extern const DisplayColor kWhiteColor = DisplayColor(
 	"White",
 	ImVec4(255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kSeashellColor = DisplayColor(
+	"Seashell",
+	ImVec4(255.0f / 255.0f, 245.0f / 255.0f, 238.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kLinenColor = DisplayColor(
+	"Linen",
+	ImVec4(233.0f / 255.0f, 220.0f / 255.0f, 240.0f / 255.0f, kColorIntensity)
+);
 
 // Blacks
 extern const DisplayColor kBlackColor = DisplayColor(
 	"Black",
 	ImVec4(0.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
 );
+extern const DisplayColor kCharcoalColor = DisplayColor(
+	"Charcoal",
+	ImVec4(54.0f / 255.0f, 69.0f / 255.0f, 79.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kMidnightColor = DisplayColor(
+	"Midnight",
+	ImVec4(25.0f / 255.0f, 25.0f / 255.0f, 112.0f / 255.0f, kColorIntensity)
+);
 
 // Browns
 extern const DisplayColor kBrownColor = DisplayColor(
 	"Brown",
 	ImVec4(165.0f / 255.0f, 42.0f / 255.0f, 42.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kMahoganyColor = DisplayColor(
+	"Mahogany",
+	ImVec4(192.0f / 255.0f, 64.0f / 255.0f, 0.0f / 255.0f, kColorIntensity)
+);
+extern const DisplayColor kBrassColor = DisplayColor(
+	"Brass",
+	ImVec4(225.0f / 255.0f, 193.0f / 255.0f, 110.0f / 255.0f, kColorIntensity)
 );
 
 } //  namespace monster_calculator
--- monster-curiosity-calculator/src/mcc_display_constants.h
+++ monster-curiosity-calculator/src/mcc_display_constants.h
@@ -7,32 +7,52 @@ namespace monster_calculator {
 // DisplayColor constants
 // Reds
 extern const DisplayColor kRedColor;
+extern const DisplayColor kVermillionColor;
+extern const DisplayColor kRubyRedColor;
 
 // Blues
 extern const DisplayColor kBlueColor;
+extern const DisplayColor kPeriwinkleColor;
+extern const DisplayColor kIndigoColor;
 
 // Greens
 extern const DisplayColor kGreenColor;
+extern const DisplayColor kCeladonColor;
+extern const DisplayColor kViridianColor;
 
 // Yellows
 extern const DisplayColor kYellowColor;
+extern const DisplayColor kSaffronColor;
+extern const DisplayColor kGoldenrodColor;
 
 // Oranges
 extern const DisplayColor kOrangeColor;
+extern const DisplayColor kPersimonColor;
+extern const DisplayColor kTangerineColor;
 
 // Purples
 extern const DisplayColor kPurpleColor;
+extern const DisplayColor kMauveColor;
+extern const DisplayColor kOrchidColor;
 
 // Pinks
 extern const DisplayColor kPinkColor;
+extern const DisplayColor kRaspberryColor;
+extern const DisplayColor kFuschiaColor;
 
 // Whites
 extern const DisplayColor kWhiteColor;
+extern const DisplayColor kSeashellColor;
+extern const DisplayColor kLinenColor;
 
 // Blacks
 extern const DisplayColor kBlackColor;
+extern const DisplayColor kCharcoalColor;
+extern const DisplayColor kMidnightColor;
 
 // Browns
 extern const DisplayColor kBrownColor;
+extern const DisplayColor kMahoganyColor;
+extern const DisplayColor kBrassColor;
 
 } //  namespace monster_calculator
