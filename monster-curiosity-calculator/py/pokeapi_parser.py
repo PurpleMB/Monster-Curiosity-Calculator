02666ae0c2fd4f722e8fe003f68f0d0ad088b4bb
PurpleMB <purplemb816@gmail.com>
2025-10-22 14:12:49 -0500

    Python: adding helper functions for api requests (synced and async)

--- monster-curiosity-calculator/py/pokeapi_parser.py
+++ monster-curiosity-calculator/py/pokeapi_parser.py
@@ -4,15 +4,26 @@ import asyncio
 import aiohttp
 import requests
 
+async def request_json_async(session, request_url):
+    async with session.get(request_url) as request_response:
+        if request_response.status != 200:
+            print(f"API Request for '{request_url}' failed")
+            print(f"Error Code: {request_response.status_code}")
+            return None
+        return await request_response.json()
+
+def request_json_synced(request_url):
+    request_response = requests.get(request_url)
+    if request_response.status_code != 200:
+        print(f"API Request for '{request_url}' failed")
+        print(f"Error Code: {request_response.status_code}")
+        return None
+    return request_response.json()
+
 def get_category_info(api_url, category_name, limit = 20000, offset = 0):
     query_url = f"{api_url}/{category_name}?limit={limit}&offset={offset}"
 
-    api_response = requests.get(query_url)
-    if api_response.status_code != 200:
-        print(f"API Request for {category_name} from {api_url} failed")
-        print(f"Error Code: {api_response.status_code}")
-        return None
-    category_info = api_response.json()
+    category_info = request_json_synced(query_url)
 
     return category_info 
 
@@ -34,11 +45,7 @@ async def form_ability_map(session, ability_info):
 # Creates the mapping of raw -> pretty name for a single ability
 async def get_ability_names(session, ability_entry):
     ability_url = ability_entry["url"]
-    async with session.get(ability_url) as ability_response:
-        if ability_response.status != 200:
-            print(f"API Request for {ability_entry["name"]} failed. Error code {ability_response.status_code}")
-            return None
-        ability_data = await ability_response.json()
+    ability_data = await request_json_async(session, ability_url)
 
     raw_name = ability_entry["name"]
 
@@ -67,11 +74,7 @@ async def form_version_map(session, version_info):
 # Creates the mapping of version-group -> generation for a single version group
 async def get_version_info(session, version_entry):
     version_url = version_entry["url"]
-    async with session.get(version_url) as version_response:
-        if version_response.status != 200:
-            print(f"API Request for {version_entry["name"]} failed. Error code {version_response.status_code}")
-            return None
-        version_data = await version_response.json()
+    version_data = await request_json_async(session, version_url)
 
     version_name = version_data["name"]
     generation_name = version_data["generation"]["name"]
@@ -83,69 +86,56 @@ async def get_version_info(session, version_entry):
 # It generates asynchronous requests gathering the data for each one before
 # gathering all request results.
 # The return of this function is a fully pruned dictionary of information.
-
-async def gather_pokemon_data(session, species_data, ability_name_map, generation_map):
+async def gather_monsters_data(session, monsters_info, ability_name_map, generation_map):
     tasks = []
-    for dex_entry in species_data["results"]:
-        task = asyncio.create_task(get_pokemon_info(session, dex_entry, ability_name_map, generation_map))
+    for monster_entry in monsters_info["results"]:
+        task = asyncio.create_task(get_monster_data(session, monster_entry, ability_name_map, generation_map))
         tasks.append(task)
+    monsters_data = await asyncio.gather(*tasks)
 
-    pokedex_data = await asyncio.gather(*tasks)
-    return pokedex_data
+    return monsters_data
 
-# This is a helper function responsible for compiling the data for one pokemon.
-# It generates async requests for info about the pokemon, its overall species,
+# This is a helper function responsible for compiling the data for one monster.
+# It generates async requests for info about the monster, its overall species,
 # and its form before passing all of that data to a pruning function.
-# The return of this function is a dictionary of prepared info for one pokemon.
-async def get_pokemon_info(session, dex_entry, ability_name_map, generation_map):
-    poke_url = dex_entry["url"]
-    async with session.get(poke_url) as poke_response:
-        if poke_response.status != 200:
-            print(f"API Request for {dex_entry["name"]} failed. Error code {poke_response.status_code}")
-            return None
-        poke_data = await poke_response.json()
+# The return of this function is a dictionary of prepared info for one monster.
+async def get_monster_data(session, monster_entry, ability_map, versions_map):
+    monster_url = monster_entry["url"]
+    monster_data = await request_json_async(session, monster_url)
 
     # Legends:ZA creatures are currently lacking data, ignore them for now
-    if poke_data["id"] > 10277:
-        return
+    if monster_data["id"] > 10277:
+        return None
 
-    species_url = poke_data["species"]["url"]
-    async with session.get(species_url) as species_response:
-        if species_response.status != 200:
-            print(f"API Request for {poke_data["species"]["name"]} failed. Error code {species_response.status_code}")
-            return None
-        species_data = await species_response.json()
+    species_url = monster_data["species"]["url"]
+    species_data = await request_json_async(session, species_url)
 
-    form_url = poke_data["forms"][0]["url"]
-    async with session.get(form_url) as form_response:
-        if form_response.status != 200:
-            print(f"API Request for {poke_data["forms"][0]["url"]} failed. Error code {form_response.status_code}")
-            return None
-        form_data = await form_response.json()
+    form_url = monster_data["forms"][0]["url"]
+    form_data = await request_json_async(session, form_url)
 
     url_data = {
-        "poke": poke_url,
+        "monster": monster_url,
         "species": species_url,
         "form": form_url
     }
 
-    pruned_species_data = prune_pokemon_info(species_data, poke_data, form_data, url_data, ability_name_map, generation_map)
-    return pruned_species_data
+    pruned_monster_data = prune_monster_info(species_data, monster_data, form_data, url_data, ability_map, versions_map)
+    return pruned_monster_data
         
 # This helper function is responsible for taking in the raw data of a
-# pokemon, its form, and its species and then re-organizing and pruning that
+# monster, its form, and its species and then re-organizing and pruning that
 # data to be convenient for later parsing into an SQL database.
-def prune_pokemon_info(species_info, poke_info, form_info, url_info, ability_name_map, generation_map):
+def prune_monster_info(species_info, monster_info, form_info, url_info, ability_map, versions_map):
     pruned_info = {}
 
 
     # basic identification info
-    pruned_info["name"] = poke_info["name"]
+    pruned_info["name"] = monster_info["name"]
     pruned_info["dex_number"] = species_info["id"]
     #pruned_info["generation"] = species_info["generation"]["name"]
     version_group = form_info["version_group"]["name"]
-    pruned_info["generation"] = generation_map[version_group]
-    pruned_info["unique_id"] = poke_info["id"]
+    pruned_info["generation"] = versions_map[version_group]
+    pruned_info["unique_id"] = monster_info["id"]
     pruned_info["form_switchable"] = 1 if species_info["forms_switchable"] else 0
     pruned_info["color"] = species_info["color"]["name"]
     pruned_info["shape"] = species_info["shape"]["name"]
@@ -183,12 +173,12 @@ def prune_pokemon_info(species_info, poke_info, form_info, url_info, ability_nam
 
     # API source info
     pruned_info["species_url"] = url_info["species"]
-    pruned_info["poke_url"] = url_info["poke"]
+    pruned_info["monster_url"] = url_info["monster"]
     pruned_info["form_url"] = url_info["form"]
 
     # breeding/growth info
     pruned_info["growth_rate"] = species_info["growth_rate"]["name"]
-    pruned_info["base_experience"] = poke_info["base_experience"]
+    pruned_info["base_experience"] = monster_info["base_experience"]
     pruned_info["base_happiness"] = species_info["base_happiness"]
     pruned_info["catch_rate"] = species_info["capture_rate"]
     pruned_info["hatch_counter"] = species_info["hatch_counter"]
@@ -205,60 +195,60 @@ def prune_pokemon_info(species_info, poke_info, form_info, url_info, ability_nam
 
     
     # classification info
-    pruned_info["is_default"] = 1 if poke_info["is_default"] else 0
+    pruned_info["is_default"] = 1 if monster_info["is_default"] else 0
     pruned_info["is_baby"] = 1 if species_info["is_baby"] else 0
     pruned_info["is_legendary"] = 1 if species_info["is_legendary"] else 0
     pruned_info["is_mythical"] = 1 if species_info["is_mythical"] else 0
     
     # size info
     # This condition is for E-max Eternatuss, which has an unknown weight of "0", I am choosing to treat its weight as very large instead for more logical sorting compared to other known weights
-    raw_weight = poke_info["weight"]
+    raw_weight = monster_info["weight"]
     if  raw_weight != 0:
         pruned_info["weight(kg)"] = raw_weight / 10
     else:
         pruned_info["weight(kg)"] = 9999.9
-    pruned_info["height(m)"] = poke_info["height"] / 10
+    pruned_info["height(m)"] = monster_info["height"] / 10
     
-    # base stat info
-    poke_stats = {}
+    # base stat info 
+    base_stats = {}
     stat_total = 0
-    for stat in poke_info["stats"]:
-        poke_stats[stat["stat"]["name"]] = stat["base_stat"]
+    for stat in monster_info["stats"]:
+        base_stats[stat["stat"]["name"]] = stat["base_stat"]
         stat_total += stat["base_stat"]
-    poke_stats["total"] = stat_total
-    pruned_info["stats"] = poke_stats
+    base_stats["total"] = stat_total
+    pruned_info["stats"] = base_stats
     
     # typing info
-    poke_types = {}
-    poke_types["primary"] = poke_info["types"][0]["type"]["name"]
-    if len(poke_info["types"]) > 1:
-        poke_types["secondary"] = poke_info["types"][1]["type"]["name"]
+    monster_types = {}
+    monster_types["primary"] = monster_info["types"][0]["type"]["name"]
+    if len(monster_info["types"]) > 1:
+        monster_types["secondary"] = monster_info["types"][1]["type"]["name"]
     else:
-        poke_types["secondary"] = "-"
-    pruned_info["types"] = poke_types
+        monster_types["secondary"] = "-"
+    pruned_info["types"] = monster_types
     
     # ability info
-    poke_abilities = {}
+    monster_abilities = {}
     abilities = ["-", "-", "-"]
-    pretty_poke_abilities = {}
+    pretty_monster_abilities = {}
     pretty_abilities = ["None", "None", "None"]
 
-    for ability in poke_info["abilities"]:
+    for ability in monster_info["abilities"]:
         ability_name = ability["ability"]["name"]
         # API contains erroneous duplicate abilities?
         if not ability_name in abilities:
             abilities[ability["slot"]-1] = ability_name
 
-    poke_abilities["first"] = abilities[0]
-    poke_abilities["second"] = abilities[1]
-    poke_abilities["hidden"] = abilities[2]
+    monster_abilities["first"] = abilities[0]
+    monster_abilities["second"] = abilities[1]
+    monster_abilities["hidden"] = abilities[2]
 
-    pretty_poke_abilities["first"] = ability_name_map[abilities[0]]
-    pretty_poke_abilities["second"] = ability_name_map[abilities[1]]
-    pretty_poke_abilities["hidden"] = ability_name_map[abilities[2]]
+    pretty_monster_abilities["first"] = ability_map[abilities[0]]
+    pretty_monster_abilities["second"] = ability_map[abilities[1]]
+    pretty_monster_abilities["hidden"] = ability_map[abilities[2]]
 
-    pruned_info["abilities"] = poke_abilities
-    pruned_info["pretty_abilities"] = pretty_poke_abilities
+    pruned_info["abilities"] = monster_abilities
+    pruned_info["pretty_abilities"] = pretty_monster_abilities
     
 
     return pruned_info
@@ -294,17 +284,17 @@ async def main():
         json.dump(versions_map, versions_file, indent = 4)
 
     # Main information gathering takes place here. 
-    species_name = "pokemon"
-    species_data = get_category_info(api_url, species_name)
+    monsters_name = "pokemon"
+    monsters_info = get_category_info(api_url, monsters_name)
     async with aiohttp.ClientSession() as session:
-        pokemon_data = await gather_pokemon_data(session, species_data, ability_map, versions_map)
+        monsters_data = await gather_monsters_data(session, monsters_info, ability_map, versions_map)
 
-        # Prune entries that failed for any reason
-        pokemon_data = list(filter(None, pokemon_data)) 
+    # Prune entries that failed for any reason
+    monsters_data = list(filter(None, monsters_data)) 
 
     monster_file_name = json_directory + "mccdata.json"
     with open(monster_file_name, "w") as monster_file:
-        json.dump(pokemon_data, monster_file, indent = 4)
+        json.dump(monsters_data, monster_file, indent = 4)
     print(f"JSON data saved to '{json_directory}'")
 
     end_time = time.time()
